d1e1427942e5f79152cfdbd58f8a5b1414b8e9c8
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-01-06 11:14:11 -0200

    Testando boleanos

---
+++ teste.php
@@ -0,0 +1,14 @@
+<p>Teste com ==</p>
+<?php
+	$res = "true" == true;
+
+	echo "ECHO: ".$res; ?><br/><?php
+	var_dump($res);
+?>
+<p>Teste com ===</p>
+<?php
+	$res = "true" === true;
+
+	echo "ECHO: ".$res; ?><br/><?php
+	var_dump($res);
+?>
