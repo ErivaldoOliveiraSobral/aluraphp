2daf4d84ce5e6557ed29b69cbe163fcc2cd4f2e9
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-01-06 10:47:13 -0200

    Inserindo veirficação de login

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php include("cabecalho.php"); ?>
+
 	<h1>Bem Vindo</h1>
 
 	<h2>Login</h2>
+	<?php
+		if (isset($_GET["login"]) && $_GET["login"] == true){
+			?>
+			<p class="alert-success">Logado com sucesso</p>
+			<?php
+		}
+		if (isset($_GET["login"]) && $_GET["login"] == false){
+			?>
+			<p class="alert-danger">Usuário ou senha inválida</p>
+			<?php
+		}
+	?>
 	<form action="login.php" method="POST">
 		<table class="table">
 			<tr>
