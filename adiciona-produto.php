3f435abf6cd5364fdfe310336aea59c333449278
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-01-11 20:02:01 -0200

    Trocando INCLUDE para REQUIRE_ONCE

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,8 +1,7 @@
 <?php 
-	include("cabecalho.php");
-	include('conecta.php');
-	include('banco-produto.php');
-	include('logica-usuario.php');
+	require_once("cabecalho.php");
+	require_once("banco-produto.php");
+	require_once("logica-usuario.php");
 
 	//Proteje para que não seja adicionado sem que o usuário estaja logado
 	verificaUsuario();
--- banco-produto.php
+++ banco-produto.php
@@ -1,39 +1,41 @@
 <?php 
-	function listaProdutos($conexao){
-		$produtos = array();
-		$result = mysqli_query($conexao,"select p.*,c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id=c.id;");
-	
-		while ($produto = mysqli_fetch_assoc($result)) {
-			array_push($produtos, $produto);
-		}
-		return $produtos;
-	}
+	require_once("conecta.php");
 
-	function insereProduto($conexao,$nome,$preco,$descricao,$categoria_id,$usado){
-		$nome = mysqli_real_escape_string($conexao, $nome);
-		$descricao = mysqli_real_escape_string($conexao, $descricao);
-		$preco = mysqli_real_escape_string($conexao, $preco);
-		//escrever query
-		$query = "insert into produtos (nome,preco,descricao,categoria_id,usado) values ('{$nome}','{$preco}','{$descricao}','{$categoria_id}','{$usado}');";
-		//enviar para o banco
-		return mysqli_query($conexao,$query);
-	}
+function listaProdutos($conexao){
+	$produtos = array();
+	$result = mysqli_query($conexao,"select p.*,c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id=c.id;");
 
-	function removeProduto($conexao,$id){
-		$query = "delete from produtos where id={$id};";
-		return mysqli_query($conexao,$query);
+	while ($produto = mysqli_fetch_assoc($result)) {
+		array_push($produtos, $produto);
 	}
+	return $produtos;
+}
 
-	function buscaProduto($conexao,$id){
-		$query = "select * from produtos where id={$id}";
-		$resultado = mysqli_query($conexao,$query);
-		return mysqli_fetch_assoc($resultado);
-	}
+function insereProduto($conexao,$nome,$preco,$descricao,$categoria_id,$usado){
+	$nome = mysqli_real_escape_string($conexao, $nome);
+	$descricao = mysqli_real_escape_string($conexao, $descricao);
+	$preco = mysqli_real_escape_string($conexao, $preco);
+	//escrever query
+	$query = "insert into produtos (nome,preco,descricao,categoria_id,usado) values ('{$nome}','{$preco}','{$descricao}','{$categoria_id}','{$usado}');";
+	//enviar para o banco
+	return mysqli_query($conexao,$query);
+}
 
-	function alteraProduto($conexao,$id,$nome,$preco,$descricao,$categoria_id,$usado){
-		$nome = mysqli_real_escape_string($conexao, $nome);
-		$descricao = mysqli_real_escape_string($conexao, $descricao);
-		$preco = mysqli_real_escape_string($conexao, $preco);
-		$query = "update produtos set nome='{$nome}',preco={$preco},descricao='{$descricao}',categoria_id={$categoria_id},usado={$usado} where id='{$id}'";
-		return mysqli_query($conexao,$query);
-	}
+function removeProduto($conexao,$id){
+	$query = "delete from produtos where id={$id};";
+	return mysqli_query($conexao,$query);
+}
+
+function buscaProduto($conexao,$id){
+	$query = "select * from produtos where id={$id}";
+	$resultado = mysqli_query($conexao,$query);
+	return mysqli_fetch_assoc($resultado);
+}
+
+function alteraProduto($conexao,$id,$nome,$preco,$descricao,$categoria_id,$usado){
+	$nome = mysqli_real_escape_string($conexao, $nome);
+	$descricao = mysqli_real_escape_string($conexao, $descricao);
+	$preco = mysqli_real_escape_string($conexao, $preco);
+	$query = "update produtos set nome='{$nome}',preco={$preco},descricao='{$descricao}',categoria_id={$categoria_id},usado={$usado} where id='{$id}'";
+	return mysqli_query($conexao,$query);
+}
